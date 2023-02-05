a5f7e18829aba1f2c7207e0d9cddfaa2c43c1c9f
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2023-02-05 11:20:07 +0000

    Begin header and navbar

--- src/home.js
+++ src/home.js
@@ -1,8 +1,8 @@
 function createHome() {
-    const title = document.createElement("div");
-    title.textContent = "1987";
+  const title = document.createElement("div");
+  title.textContent = "1987";
 
-    return title;
+  return title;
 }
 
 export default createHome;
--- src/site.js
+++ src/site.js
@@ -1,4 +1,34 @@
 import createHome from "./home";
 
 const contentDiv = document.getElementById("content");
-contentDiv.appendChild(createHome());
+const navItems = ["Home", "Menu", "About", "Booking"];
+let activatedItem = 0;
+
+function header() {
+  const headerDiv = document.createElement("div");
+  headerDiv.id = "header";
+
+  const logo = document.createElement("div");
+  logo.id = "logo";
+  logo.textContent = "1987";
+  logo.classList.add("navItem");
+  headerDiv.appendChild(logo);
+
+  const navBar = document.createElement("div");
+  navBar.id = "navBar";
+  for (const item of navItems) {
+    const navItem = document.createElement("div");
+    navItem.id = "nav" + item;
+    navItem.textContent = item;
+    navItem.classList.add("navItem");
+    if (item === navItems[activatedItem]) {
+        navItem.classList.add("activated");
+    }
+    navBar.appendChild(navItem);
+  }
+  headerDiv.appendChild(navBar);
+
+  return headerDiv;
+}
+
+contentDiv.appendChild(header());
